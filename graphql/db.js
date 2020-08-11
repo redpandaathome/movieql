a1db36bc91bc9de5df1319e9e309344dd0b26383
yumi <yumi@yumiui-MacBookPro.local>
2020-08-11 11:39:03 +0900

    test movie mutation from array db

--- graphql/db.js
+++ graphql/db.js
@@ -1,20 +1,39 @@
-export const people = [
+export let movies = [
     {
-        id: "1",
-        name: "Nico",
-        age: "10",
-        gender: "None"
+        id: 0,
+        name: "Star Wars = The new one",
+        score: "1"
     },
     {   
-        id: "2",
-        name: "Neko",
-        age: "100",
-        gender: "None"
+        id: 1,
+        name: "Avengers - The new one",
+        score: "8"
     }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filteredPeople =people.filter(person=>String(id) === person.id);
-    return filteredPeople[0];
+    const filteredMovie =movies.filter(movie=> movie.id === id);
+    return filteredMovies[0];
 }
 
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie=> movie.id !== id);
+    if(movies.length >= cleanedMovies.length){
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length +1}`,
+        name,
+        score
+    }
+    movies.push(newMovie);
+    return newMovie;
+}
